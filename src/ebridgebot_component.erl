50c1c89353bb230c0350e8f7c81e35dd14a50816
cryoflamer <cryoflamer22@gmail.com>
2023-03-01 18:00:57 +0200

    upd ebridgebot_component

--- src/ebridgebot_component.erl
+++ src/ebridgebot_component.erl
@@ -1,7 +1,8 @@
 -module(ebridgebot_component).
 -export([init/1, handle_info/3, process_stanza/3, terminate/2]).
 
-init(_Args) ->
+init(Args) ->
+	ct:print("!!init(~p)", [Args]),
 	{ok, []}.
 
 %% Function that handles information message Info received
@@ -9,7 +10,7 @@ init(_Args) ->
 handle_info(Info, Client, State) ->
 	%% Here you can implement the handling of the information message
 	%% and change the State accordingly
-	io:format("~n!!handle_info(~p, ~p, ~p) ~n", [Info, Client, State]),
+	ct:print("!!handle_info(~p, ~p, ~p)", [Info, Client, State]),
 	{ok, State}.
 
 %% Function that processes the Stanza from the Client with the
@@ -17,9 +18,9 @@ handle_info(Info, Client, State) ->
 process_stanza(Stanza, Client, State) ->
 	%% Here you can implement the processing of the Stanza and
 	%% change the State accordingly
-	io:format("~n!@!process_stanza(~p, ~p, ~p) ~n", [Stanza, Client, State]),
+	ct:print("!@!process_stanza(~p, ~p, ~p)", [Stanza, Client, State]),
 	{ok, State}.
 
 terminate(Reason, State) ->
-	io:format("~n!#!terminate/2 ~p~n", [{Reason, State}]),
+	ct:print("!#!terminate/2 ~p", [{Reason, State}]),
 	component_stopped.
