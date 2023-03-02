460a6978762af9fc350c504fe79ce67f73b0577e
cryoflamer <cryoflamer22@gmail.com>
2023-03-02 14:50:30 +0200

    add ebridgebot:link_room fun

--- src/ebridgebot.erl
+++ src/ebridgebot.erl
@@ -2,3 +2,6 @@
 -compile(export_all).
 
 -include("ebridgebot.hrl").
+
+link_room(GroupId, MucJid, BotName) ->
+	mnesia:dirty_write(#ebridgebot_muc{group_id = GroupId, muc_jid = MucJid, bot_name = BotName}).
