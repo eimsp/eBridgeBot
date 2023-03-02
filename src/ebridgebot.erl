6fb28aa2da24123c446fc29146ce431fb9aff909
cryoflamer <cryoflamer22@gmail.com>
2023-03-02 21:17:55 +0200

    add sys.config and ebridgebot:run_test fun

--- src/ebridgebot.erl
+++ src/ebridgebot.erl
@@ -5,3 +5,22 @@
 
 link_room(GroupId, MucJid, BotName) ->
 	mnesia:dirty_write(#ebridgebot_muc{group_id = GroupId, muc_jid = MucJid, bot_name = BotName}).
+
+run_test() ->
+	run_test(ebridgebot_component_SUITE, []).
+run_test(Suite) ->
+	run_test(Suite, []).
+run_test(Suite, Testcase) when is_atom(Testcase) ->
+	run_test(Suite, [Testcase]);
+run_test(Suite, Testcases) when is_atom(Suite) ->
+	case application:get_env(ebridgebot, test_path, []) of
+		[] -> ct:print("ERROR: test_path not found. Set ederibit_path in ejabberd section of sys.config", []),
+			{error, invalid_deribit_path};
+		DeribitPath ->
+			ct:run_test(
+				[{testcase, T} || T <- Testcases] ++
+				[{suite, Suite},
+					{dir, filename:join(DeribitPath, "test")},
+					{include, ["_build/dev/lib/escalus", "include"]},
+					{config, filename:join(DeribitPath, "test/test.config")}])
+	end.
