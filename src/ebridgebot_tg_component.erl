3ef5eed6599c086fa9c913b5ccba4b18e498a964
cryoflamer <cryoflamer22@gmail.com>
2023-03-04 15:24:53 +0200

    add enter_linked_rooms fun and presence handling on response

--- src/ebridgebot_tg_component.erl
+++ src/ebridgebot_tg_component.erl
@@ -19,12 +19,14 @@
 
 init(Args) ->
 	application:ensure_all_started(pe4kin),
-	[BotId, BotName, BotToken, Component, Nick, Token] = [proplists:get_value(K, Args) ||
-		K <- [bot_id, name, token, component, nick, token]], %% TODO unused params need remove in future
+	[BotId, BotName, BotToken, Component, Nick, Token, Rooms] =
+		[proplists:get_value(K, Args) ||
+		 K <- [bot_id, name, token, component, nick, token, linked_rooms]], %% TODO unused params need remove in future
 	pe4kin:launch_bot(BotName, BotToken, #{receiver => true}),
 	pe4kin_receiver:subscribe(BotName, self()),
 	pe4kin_receiver:start_http_poll(BotName, #{limit=>100, timeout=>60}),
-	{ok, #tg_state{bot_id = BotId, bot_name = BotName, component = Component, nick = Nick, token = Token}}.
+	NewRooms = [#muc_state{group_id = TgId, muc_jid = MucJid} || {TgId, MucJid} <- Rooms],
+	{ok, #tg_state{bot_id = BotId, bot_name = BotName, component = Component, nick = Nick, token = Token, rooms = NewRooms}}.
 
 %% Function that handles information message received from the group chat of Telegram
 handle_info({pe4kin_update, BotName,
@@ -44,6 +46,18 @@ handle_info({enter_groupchat, MucJid}, Client, #tg_state{component = Component, 
 	EnterPresence = #presence{from = jid:make(Component), to = jid:replace_resource(jid:decode(MucJid), Nick), sub_els = [#muc{}]},
 	escalus:send(Client, xmpp:encode(EnterPresence)),
 	{ok, State};
+handle_info(enter_linked_rooms, Client, #tg_state{rooms = Rooms} = State) ->
+	NewRooms =
+		lists:foldr(
+			fun(#muc_state{muc_jid = MucJid, state = out} = MucState, Acc) ->
+				case lists:keyfind(MucJid, #muc_state.muc_jid, Acc) of
+					#muc_state{} -> ok;
+					false -> handle_info({enter_groupchat, MucJid}, Client, State)
+				end,
+				[MucState#muc_state{state = pending} | Acc];
+				(MucState, Acc) -> [MucState | Acc]
+			end, [], Rooms),
+	{ok, State#tg_state{rooms = NewRooms}};
 handle_info({state, Pid}, _Client, State) ->
 	Pid ! {state, State},
 	{ok, State};
@@ -51,9 +65,25 @@ handle_info(Info, _Client, State) ->
 	ct:print("handle component: ~p", [Info]),
 	{ok, State}.
 
-%% Function that processes the Stanza from the Client with the
-%% UserState state of the user
-process_stanza(Stanza, Client, State) ->
+process_stanza(#xmlel{} = Stanza, Client, State) ->
+	process_stanza(xmpp:decode(Stanza), Client, State);
+process_stanza(#presence{type = available, from = #jid{} = CurMucJID, to = To} = Pkt,
+	_Client, #tg_state{rooms = Rooms, component = ComponentJid} = State) ->
+%%	ct:print("handle component presence: ~p", [Pkt]),
+	case {jid:encode(To), xmpp:get_subtag(Pkt, #muc_user{})} of
+		{ComponentJid, #muc_user{items = [#muc_item{jid = To}]}} ->
+			CurMucJid = jid:encode(jid:remove_resource(CurMucJID)),
+			NewRooms =
+				lists:foldr(
+					fun(#muc_state{muc_jid = MucJid} = MucState, Acc) when MucJid == CurMucJid ->
+							[MucState#muc_state{state = in} | Acc]; %% set muc_state as in for this presence
+					   (MucState, Acc) ->
+						    [MucState | Acc]
+					end, [], Rooms),
+			{ok, State#tg_state{rooms = NewRooms}};
+		_ -> {ok, State}
+	end;
+process_stanza(Stanza, _Client, State) ->
 	%% Here you can implement the processing of the Stanza and
 	%% change the State accordingly
 	ct:print("handle component stanza: ~p", [Stanza]),
@@ -83,3 +113,6 @@ pid(BotId) ->
 
 enter_groupchat(BotId, MucJid) ->
 	pid(BotId) ! {enter_groupchat, MucJid}.
+
+enter_linked_rooms(BotId) ->
+	pid(BotId) ! enter_linked_rooms.
