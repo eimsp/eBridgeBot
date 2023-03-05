2f977e784058b13b7918d47e8fbdc1cd04645855
cryoflamer <cryoflamer22@gmail.com>
2023-03-05 16:19:28 +0200

    add ebridgebot:subscribe_component/2

--- src/ebridgebot_tg_component.erl
+++ src/ebridgebot_tg_component.erl
@@ -27,6 +27,7 @@ init(Args) ->
 	pe4kin_receiver:subscribe(BotName, self()),
 	pe4kin_receiver:start_http_poll(BotName, #{limit=>100, timeout=>60}),
 	NewRooms = [#muc_state{group_id = TgId, muc_jid = MucJid} || {TgId, MucJid} <- Rooms],
+%%	self() ! sub_linked_rooms, %% subscribe to all linked rooms
 	self() ! enter_linked_rooms, %% enter to all linked rooms
 	{ok, #tg_state{bot_id = BotId, bot_name = BotName, component = Component, nick = Nick, token = Token, rooms = NewRooms}}.
 
@@ -43,7 +44,7 @@ handle_info({pe4kin_update, BotName,
 			 Uuid = ebridgebot:gen_uuid(),
 			 escalus:send(Client, xmpp:encode(#message{id = Uuid, type = groupchat, from = jid:decode(Component), to = jid:decode(MucJid),
 				 body = [#text{data = <<TgUserName/binary, ":\n", Text/binary>>}], sub_els = [#origin_id{id = Uuid}]})), Uuid
-		 end || #muc_state{group_id = TgId, muc_jid = MucJid} <- Rooms, Id == TgId],
+		 end || #muc_state{group_id = TgId, muc_jid = MucJid, state = S} <- Rooms, Id == TgId andalso (S == in orelse S == subscribed)],
 	{ok, State#tg_state{ignored_ids = Ids ++ IgnoredIds}};
 handle_info({pe4kin_update, BotName, #{<<"message">> := _} = TgMsg}, _Client, #tg_state{bot_name = BotName} = State) ->
 	ct:print("tg msg: ~p", [TgMsg]),
@@ -63,6 +64,10 @@ handle_info({enter_groupchat, MucJid}, Client, #tg_state{component = Component, 
 	EnterPresence = #presence{from = jid:make(Component), to = jid:replace_resource(jid:decode(MucJid), Nick), sub_els = [#muc{}]},
 	escalus:send(Client, xmpp:encode(EnterPresence)),
 	{ok, State};
+handle_info({subscribe_component, MucJid}, Client, #tg_state{component = Component, nick = Nick} = State) when is_binary(MucJid) ->
+	ct:print("subscribe_component: ~p", [MucJid]),
+	escalus:send(Client, xmpp:encode(sub_iq(jid:make(Component), jid:decode(MucJid), Nick))),
+	{ok, State};
 handle_info(enter_linked_rooms, Client, #tg_state{rooms = Rooms} = State) ->
 	NewRooms =
 		lists:foldr(
@@ -72,6 +77,19 @@ handle_info(enter_linked_rooms, Client, #tg_state{rooms = Rooms} = State) ->
 						false -> handle_info({enter_groupchat, MucJid}, Client, State)
 					end,
 					[MucState#muc_state{state = pending} | Acc];
+				(MucState, Acc) ->
+					[MucState | Acc]
+			end, [], Rooms),
+	{ok, State#tg_state{rooms = NewRooms}};
+handle_info(sub_linked_rooms, Client, #tg_state{rooms = Rooms} = State) ->
+	NewRooms =
+		lists:foldr(
+			fun(#muc_state{muc_jid = MucJid, state = out} = MucState, Acc) ->
+					case lists:keyfind(MucJid, #muc_state.muc_jid, Acc) of
+						#muc_state{} -> ok;
+						false -> handle_info({subscribe_component, MucJid}, Client, State)
+					end,
+					[MucState#muc_state{state = subscribed} | Acc];
 				(MucState, Acc) ->
 					[MucState | Acc]
 			end, [], Rooms),
@@ -100,16 +118,19 @@ process_stanza(#presence{type = available, from = #jid{} = CurMucJID, to = To} =
 			{ok, State#tg_state{rooms = NewRooms}};
 		_ -> {ok, State}
 	end;
-process_stanza(#message{id = Id, type = groupchat, from = #jid{resource = Nick} = From, body = [#text{data = Text}]} = _Pkt, _Client,
+process_stanza(#message{id = Id, type = groupchat, from = #jid{resource = Nick} = From, body = [#text{data = Text}]} = Pkt, _Client,
 	#tg_state{bot_name = BotName, rooms = Rooms, ignored_ids = IgnoredIds} = State) ->
 	MucFrom = jid:encode(jid:remove_resource(From)),
 	NewState =
 		case lists:delete(Id, IgnoredIds) of
 			IgnoredIds ->
+				ct:print("msg to tg: ~p", [Pkt]),
 				[pe4kin:send_message(BotName, #{chat_id => TgId, text => <<Nick/binary, ":\n", Text/binary>>})
 					|| #muc_state{muc_jid = MucJid, group_id = TgId} <- Rooms, MucFrom == MucJid],
 				State;
-			Ids -> State#tg_state{ignored_ids = Ids}
+			Ids ->
+				ct:print("ignored msg to tg: ~p", [Pkt]),
+				State#tg_state{ignored_ids = Ids}
 		end,
 	{ok, NewState};
 process_stanza(Stanza, _Client, State) ->
@@ -148,3 +169,16 @@ enter_linked_rooms(BotId) ->
 
 send(BotId, ChatId, Text) ->
 	pid(BotId) ! {pe4kin_send, ChatId, Text}.
+
+sub_iq(From, To, Nick) ->
+	sub_iq(From, To, Nick, <<>>).
+sub_iq(From, To, Nick, Password) ->
+	#iq{type = set, from = From, to = To,
+		sub_els = [#muc_subscribe{nick = Nick, password = Password,
+			events = [?NS_MUCSUB_NODES_MESSAGES,
+				?NS_MUCSUB_NODES_AFFILIATIONS,
+				?NS_MUCSUB_NODES_SUBJECT,
+				?NS_MUCSUB_NODES_CONFIG]}]}.
+
+subscribe_component(BotId, MucJid) ->
+	pid(BotId) ! {subscribe_component, MucJid}.
