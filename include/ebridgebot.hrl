0e8121c8558eaf88417bef75484be1f3bc28443f
cryoflamer <cryoflamer22@gmail.com>
2023-03-08 11:31:12 +0200

    add ?dbg alias to ebridgebot.hrl

--- include/ebridgebot.hrl
+++ include/ebridgebot.hrl
@@ -1,3 +1,9 @@
+-define(dbg(Fmt, Args),
+	case xmpp_config:debug(global) of
+		{ok, true} -> error_logger:info_msg(Fmt, Args);
+		_ -> false
+	end).
+
 -record(xmpp_link, {
 	time = erlang:system_time(microsecond) :: integer(),
 	xmpp_id = [] :: binary(),
