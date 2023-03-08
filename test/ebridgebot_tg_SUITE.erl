24f29627aa99e0f4ff469d6a1b61370e53064e57
cryoflamer <cryoflamer22@gmail.com>
2023-03-08 18:30:54 +0200

    add muc_story test

--- test/ebridgebot_tg_SUITE.erl
+++ test/ebridgebot_tg_SUITE.erl
@@ -18,8 +18,11 @@
 -include_lib("common_test/include/ct.hrl").
 -include_lib("exml/include/exml.hrl").
 -include_lib("xmpp/include/xmpp_codec.hrl").
+-include("ebridgebot.hrl").
+-include("ebridgebot_tg.hrl").
 
--import(ebridgebot_component_SUITE, [get_property/2]).
+-import(ebridgebot_component_SUITE, [get_property/2, enter_room/3]).
+-import(ebridgebot, [wait_for_result/2, wait_for_result/4, wait_for_list/1, wait_for_list/2]).
 
 -define(BOT_ID, test_tg_bot).
 -define(NICK, atom_to_binary(?BOT_ID)).
@@ -28,16 +31,14 @@ all() ->
 	[{group, main}].
 
 groups() ->
-	MainStories = [room_story],
+	MainStories = [muc_story],
 	[{main, [sequence], MainStories}, {local, [sequence], MainStories}].
 
 init_per_suite(Config) ->
 	[escalus:Fun([{escalus_user_db, {module, escalus_ejabberd}} | Config], escalus_users:get_users([alice])) || Fun <- [delete_users, create_users]],
-	MucHost = escalus_ct:get_config(muc_host),
-	RoomNode = escalus_config:get_ct({ebridgebot_rooms, ebridgebot_test, name}),
-	ebridgebot_component_SUITE:init_per_testcase(ebridgebot_tg_SUITE, Config), %% TODO to move add linking room to testcase in future
+	application:stop(ebridgebot),
 	Args = [{bot_id, ?BOT_ID},
-		{component, <<"tg.localhost">>}, %% TODO move to test.config
+		{component, escalus_ct:get_config(ejabberd_service)}, %% TODO move to test.config
 		{name, <<"ebridge_bot">>}, %% TODO change for test name and insert meck
 		{host, escalus_ct:get_config(ejabberd_addr)},
 		{nick, ?NICK},
@@ -45,19 +46,21 @@ init_per_suite(Config) ->
 		{module, ebridgebot_tg_component}, %% TODO set ebridgebot_tg module in future
 		{port, escalus_ct:get_config(ejabberd_service_port)},
 		{token, <<"6066841531:AAEK0aUdaP6eoJWcS0020VOyYQpNhhMpBPE">>}, %% TODO set fake token in future by meck
-		{linked_rooms, [{-930545885, jid:to_string({RoomNode, MucHost, <<>>})}]}], %% TODO set fake tg room id in future by meck
-	{ok, Pid} = escalus_component:start({local, get_property(bot_id, Args)}, get_property(module, Args),
-		Args, Args),
+		{linked_rooms, []}],
+	{ok, Pid} = escalus_component:start({local, get_property(bot_id, Args)}, get_property(module, Args), Args, Args),
 	Args ++ [{component_pid, Pid} | escalus:init_per_suite(Config)].
 
 end_per_suite(Config) ->
 	ok = ebridgebot_tg_component:stop(get_property(component_pid, Config)),
+	application:start(ebridgebot),
 	escalus:end_per_suite(Config).
 
 %%init_per_testcase(test_msg, Config) ->
 %%	meck:new(ebridgebot_tg_component),
 %%%%	meck:expect(ebridgebot_tg_component, process_stanza, fun process_stanza/3),
 %%	escalus:init_per_testcase(test_msg, Config);
+%%init_per_testcase(muc_story, Config) ->
+%%	ebridgebot_component_SUITE:init_per_testcase(CaseName, Config).
 init_per_testcase(CaseName, Config) ->
 	ebridgebot_component_SUITE:init_per_testcase(CaseName, Config).
 
@@ -71,5 +74,25 @@ end_per_testcase(CaseName, Config) ->
 	{ok, atomic} = mnesia:delete_table(ebridgebot_tg_component:bot_table(BotId)),
 	escalus:end_per_testcase(CaseName, Config).
 
-room_story(Config) ->
-	ok.
+muc_story(Config) ->
+	[RoomNode, ChatId] = [escalus_config:get_ct({ebridgebot_rooms, ebridgebot_test, K}) || K <- [name, chat_id]],
+	MucHost = escalus_config:get_ct(muc_host),
+	RoomJid = jid:to_string({RoomNode, MucHost, <<>>}),
+	AliceNick = escalus_config:get_ct({escalus_users, alice, nick}),
+	BotId = get_property(bot_id, Config),
+	[Pid] = [get_property(Key, Config) || Key <- [component_pid]],
+	#tg_state{bot_id = BotId, rooms = []} = ebridgebot_tg_component:state(Pid),
+	escalus:story(Config, [{alice, 1}],
+		fun(#client{jid = _AliceJid} = Alice) ->
+			enter_room(Alice, RoomJid, AliceNick),
+			escalus_client:wait_for_stanzas(Alice, 1),
+			Pid ! {link_rooms, ChatId, RoomJid},
+			#tg_state{bot_id = BotId, rooms = [#muc_state{group_id = ChatId, state = out}]} = ebridgebot_tg_component:state(Pid),
+			Pid ! enter_linked_rooms,
+			#tg_state{bot_id = BotId, rooms = [#muc_state{group_id = ChatId, state = pending}]} = ebridgebot_tg_component:state(Pid),
+			escalus_client:wait_for_stanzas(Alice, 1),
+			#tg_state{bot_id = BotId, rooms = [#muc_state{state = in}]} =
+				wait_for_result(fun() -> ebridgebot_tg_component:state(Pid) end,
+					fun(#tg_state{rooms = [#muc_state{state = in}]}) -> true; (_) -> false end),
+			ok
+		end).
