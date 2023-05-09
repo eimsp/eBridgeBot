5ddac516ada87860e435518efa73d64c6a12792b
cryoflamer <cryoflamer22@gmail.com>
2023-05-10 00:39:25 +0300

    remove ebridgebot:send fun

--- src/ebridgebot.erl
+++ src/ebridgebot.erl
@@ -143,30 +143,6 @@ to_rooms(CurChatId, Rooms, Fun) ->
 	[Fun(ChatId, MucJid) || #muc_state{group_id = ChatId, muc_jid = MucJid, state = {E, S}} <- Rooms,
 		CurChatId == ChatId andalso (E == in orelse S == subscribed)].
 
--spec send(msg | edit_msg, pid() | {escalus | escalus_component, term()}, atom(), binary(), binary(), any(), binary(), binary()) -> ok.
-send(SendType, Pid, BotId, From, To, Uid, Nick, Text) ->
-	send(SendType, Pid, BotId, From, To, Uid, Nick, Text, []).
-
--spec send(msg | edit_msg, pid() | {escalus | escalus_component, term()}, atom(), binary(), binary(), any(), binary(), binary(), list(term())) -> ok.
-send(SendType, Pid, BotId, From, To, Uid, Nick, Text, SubEls) when is_pid(Pid) ->
-	send(SendType, {escalus_component, Pid}, BotId, From, To, Uid, Nick, Text, SubEls);
-send(msg, {Module, Client}, BotId, From, To, Uid, Nick, Text, SubEls) when Module == escalus; Module == escalus_component ->
-	OriginId = ebridgebot:gen_uuid(),
-	Module:send(Client, xmpp:encode(#message{id = OriginId, type = groupchat, from = jid:decode(From), to = jid:decode(To),
-		body = [#text{data = <<?NICK(Nick), Text/binary>>}], sub_els = [#origin_id{id = OriginId} | SubEls]})),
-	ebridgebot:write_link(BotId, OriginId, Uid);
-send(edit_msg, {Module, Client}, BotId, From, To, Uid, Nick, Text, SubEls) when Module == escalus; Module == escalus_component ->
-	case ebridgebot:index_read(BotId, Uid, #xmpp_link.uid) of
-		[#xmpp_link{origin_id = ReplaceId, uid = Uid} | _] ->
-			Pkt = #message{id = OriginId} = ebridgebot:edit_msg(jid:decode(From), jid:decode(To),
-				<<?NICK(Nick), Text/binary>>, ReplaceId, SubEls),
-			Module:send(Client, xmpp:encode(Pkt)),
-			ebridgebot:write_link(BotId, OriginId, Uid); %% TODO maybe you don't need to write because there is no retract from Telegram
-		_ -> ok
-	end;
-send(SendType, Client, BotId, From, To, Uid, Nick, Text, Els) when SendType == msg; SendType == edit_msg ->
-	send(SendType, {escalus, Client}, BotId, From, To, Uid, Nick, Text, Els).
-
 merge_entities(Entities) ->
 	lists:flatten(tuple_to_list(
 		lists:foldl(
